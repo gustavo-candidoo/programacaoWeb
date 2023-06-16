ecd19122860679e922cf371490766a50eb12571c
Gustavo Candido <luiz.candido@acad.espm.br>
2023-06-15 21:03:46 -0300

    ex02

--- ex02/ex02.js
+++ ex02/ex02.js
@@ -1,22 +1,41 @@
 let msg = document.getElementById("mensagem")
 let senha = ""
-let campoSenha = document.getElementById("senha")
+let inSenha = document.getElementById("inSenha")
+let humberto = document.getElementById("telaHumberto")
 
 function adicionarNumero(num){
     senha += num
-    passwordField.value = "*".repeat(password.length);
+    inSenha.value = "*".repeat(senha.length)
 }
 
 function validar(){
     if(senha === "1234"){
-        msg.innerHTML = "Senha correta! Acesso permitido."
+        mostrarTelaHumberto()
     }
-    return msg.innerHTML = "Senha incorreta! Acesso negado."
+    else{
+        msg.innerHTML = "Senha incorreta! Tente novamente."
+    }
 }
+
+function mostrarTelaHumberto() {
+    humberto.style.display = "block";
+}
+
+function limparSenha() {
+    senha = ""
+    inSenha.value = ""
+  }
 
 document.addEventListener("keydown", function(event) {
     const num = event.key;
     if (/[0-9]/.test(num)) {
-      adicionarNumero(num);
+      adicionarNumero(num)
     }
   })
+
+document.addEventListener("keyup", function(event) {
+    const key = event.key;
+    if (key === "Enter") {
+        validar();
+    }
+    })
