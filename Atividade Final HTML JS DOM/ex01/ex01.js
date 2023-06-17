7e3e1514d0d80bb41661c072d99711b7ac3c25b6
Gustavo Candido <lg0702@hotmail.com>
2023-06-16 23:21:50 -0300

    ex01

---
+++ Atividade Final HTML JS DOM/ex01/ex01.js
@@ -0,0 +1,63 @@
+async function pesquisar(){
+    let html = ""
+
+    let response = await fetch('https://academico.espm.br/testeapi/jogo/criar')
+
+    let armazenar = await response.json()
+
+    for(let i = 0; i < armazenar.length; i++){
+        let jogo = armazenar[i]
+
+        html += `
+            <p>${jogo.nome}</p>
+            <p>${jogo.descricao}</p>
+            <p>${jogo.produtora}</p>
+            <p>${jogo.ano}</p>
+            <p>${jogo.idadeMinima}</p>
+        `
+
+        let div = document.getElementById("divJogos")
+        div.innerHTML = html
+    }
+}
+
+async function criar(){
+
+    let jogo = {
+        "nome": "The Last of Us",
+        "descricao": "Zombie Game",
+        "produtora": "Naughty Dog",
+        "ano": "2013",
+        "idadeMinima": "18"
+    }
+
+    let response = await fetch ("https://academico.espm.br/testeapi/jogo/criar", {
+        method: "post",
+        body: JSON.stringify(jogo),
+        headers: {
+            "content-type": "application/json"
+        }
+    })
+
+    if(response.ok){
+        alert("OK")
+    }
+    else{
+        alert("Erro")
+    }
+}
+
+async function excluir(jogoId) {
+    let url = `https://academico.espm.br/testeapi/jogo/${jogoId}`;
+  
+    let response = await fetch(url, {
+      method: "DELETE"
+    });
+  
+    if (response.ok) {
+      alert("Jogo excluído com sucesso");
+    } else {
+      alert("Erro ao excluir o jogo");
+    }
+  }
+  
